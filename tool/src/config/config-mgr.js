f13e59af7e31bce0a1f82189c25fb0b0755260b5
mohits-git <mohitkhatri2805@gmail.com>
2024-05-26 16:34:44 +0530

    feat: added 'ajv' for JSON schema validation for correct configuration

--- tool/src/config/config-mgr.js
+++ tool/src/config/config-mgr.js
@@ -1,9 +1,23 @@
 const { cosmiconfigSync } = require('cosmiconfig');
+const chalk = require('chalk');
+const schema = require('./schema.json');
+const Ajv = require('ajv').default;
+const betterAjvErrors = require('better-ajv-errors').default;
+
+const ajv = new Ajv();
+
 const configLoader = cosmiconfigSync('tool');
 
 module.exports = function getConfig() {
     const result = configLoader.search(process.cwd());
     if (result) {
+        const isValid = ajv.validate(schema, result.config);
+        if(!isValid) {
+            console.log(chalk.yellow('Invalid configuration'));
+            console.log('');
+            console.log(betterAjvErrors(schema, result.config, ajv.errors));
+            process.exit(1);
+        }
         console.log("Found Configuration: ", result.config);
         return result.config;
     } else {
